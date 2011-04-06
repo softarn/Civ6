d4415de98e0c861039aab34970d7d09d6e6bb261
unknown <marc-hoj@.dsv.su.se>
2011-04-06 10:47:58 +0200

    Added GameMap, TerrainType and Tile. Created stubs for methods.

---
+++ src/TerrainType.java
@@ -0,0 +1,12 @@
+class TerrainType {
+	String name;
+	int attackBonus;
+	int defenceBonus;
+	
+	TerrainType(String name, int att, int def){
+		this.name = name;
+		attackBonus = att;
+		defenceBonus = def;
+	}
+
+}
