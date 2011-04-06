7bb5d595e481908e5cb45b1c876d7d2ce80d4b50
stinamard <stinamard@hotmail.com>
2011-04-06 14:09:53 +0200

    Created Window.java with constructor for size and position. Created TileView.java with getTilePosition. Also created menu.java, with nothing in it for the moment.

---
+++ src/TileView.java
@@ -0,0 +1,14 @@
+public class TileView{
+	
+	public int positionx;
+	public int positiony;
+	
+	public int getTilePositionx(){
+		return positionx;
+	}
+	
+	public int getTilePositiony(){
+		return positiony;
+	}
+
+}
---
+++ src/Window.txt
@@ -0,0 +1,13 @@
+public class Window{
+	int x;
+	int y;
+	int w;
+	int h;
+	
+	Window (int x,int y,int w,int h){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+}
