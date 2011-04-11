e923b546d0e37717610864120fd10317f7ea2ba8
Marcus Höjvall <softarn@gmail.com>
2011-04-11 09:17:23 +0200

    Added state/State.java

---
+++ src/state/State.java
@@ -0,0 +1,46 @@
+package state;
+
+import java.util.Observable;
+
+public class State extends Observable{
+    private static final State INSTANCE = new State();
+    private UnitState unitState;
+    private ActionState actionState;
+
+    private State(){
+	setUnitState(UnitState.UnSelected);
+	setActionState(ActionState.None);
+    }
+
+    public static State getInstance(){
+	return INSTANCE;
+    }
+
+    public enum UnitState{
+	Selected, UnSelected 
+    }
+
+    public UnitState getUnitState(){
+	return unitState;
+    }
+
+    public void setUnitState(UnitState state){
+	unitState = state;
+	setChanged();
+	notifyObservers();
+    }
+
+    public enum ActionState{
+	None, Move 
+    }
+
+    public ActionState getActionState(){
+	return actionState;
+    }
+
+    public void setActionState(ActionState state){
+	actionState = state;
+	setChanged();
+	notifyObservers();
+    }
+}
