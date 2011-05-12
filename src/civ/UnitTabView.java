b19e365eabc159231b7c5c7b019295c791b9dc8b
Jeppe B <silversvarnad@hotmail.com>
2011-05-12 09:21:32 +0200

    TabbedPane enlarged

--- src/civ/UnitTabView.java
+++ src/civ/UnitTabView.java
@@ -5,24 +5,59 @@ import java.awt.*;
 
 class UnitTabView extends JPanel{
 	private JTabbedPane unitTypePane = new JTabbedPane();
-	private JPanel viewAllPanel= new JPanel();
+	private JPanel viewAllPanel = new JPanel();
 	private JLabel enhetsNamn = new JLabel();
+	private Tile t = new Tile(); 
+	//private PhyiscalUnit pu = new PhysicalUnit();
+	
+	//private ArrayList<PhysicalUnit> allUnitsArray = new ArrayList<PhysicalUnit> ();
+	//private GameMap gm = new GameMap(null);
 	
 	public UnitTabView(){ 
 		setLayout(new BorderLayout());
 
-		unitTypePane.addTab("Alla ", null, null, "Visa alla enheter ");
-		unitTypePane.addTab("Fotburna ", null, null, "Visa fotburna enhter ");
-		unitTypePane.addTab("Ryttare ", null, null, "Visa beridna enheter ");
-		unitTypePane.addTab("Båtar ", null, null, "Visa båtarburna enheter ");
-		unitTypePane.addTab("Artilleri ", null, null, "Visa artillerienheter ");
-		unitTypePane.addTab("Civila ", null, null, "Visa civila enheter ");
+		unitTypePane.addTab("<html><body leftmargin=15 topmargin=12 marginwidth=15 marginheight=10>Alla </body></html>", null, viewAllPanel, "Visa alla enheter ");
+		unitTypePane.addTab("<html><body leftmargin=15 topmargin=12 marginwidth=15 marginheight=10>Fotburna </body></html>", null, null, "Visa fotburna enhter ");
+		unitTypePane.addTab("<html><body leftmargin=15 topmargin=12 marginwidth=15 marginheight=10>Ryttare </body></html>", null, null, "Visa beridna enheter ");
+		unitTypePane.addTab("<html><body leftmargin=15 topmargin=12 marginwidth=15 marginheight=10>Båtar </body></html>", null, null, "Visa båtarburna enheter ");
+		unitTypePane.addTab("<html><body leftmargin=15 topmargin=12 marginwidth=15 marginheight=10>Artilleri </body></html> ", null, null, "Visa artillerienheter ");
+		unitTypePane.addTab("<html><body leftmargin=15 topmargin=12 marginwidth=15 marginheight=10>Civila </body></html>", null, null, "Visa civila enheter ");
 
-				
 		//viewAllPanel.add();	
+		
+		//this.printUnitArray();
 		
 		add(unitTypePane, BorderLayout.NORTH);
 		
 	}
 	
+	/*public void printUnitArray() {
+		for (PhysicalUnit pu : t.getUnitArray()){
+			enhetsNamn.setText(pu.toString());		
+		}
+		
+		viewAllPanel.add(enhetsNamn);
+		unitTypePane.add(viewAllPanel);
+		unitTypePane.repaint();
+	}*/
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
